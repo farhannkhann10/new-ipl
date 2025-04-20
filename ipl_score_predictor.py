79cd8d34646b57c0a154c2af4584190c4c481a10
farhannkhann10 <khanfarhan7487@gmail.com>
2025-04-20 14:59:07 +0530

    Update ipl_score_predictor.py

--- ipl_score_predictor.py
+++ ipl_score_predictor.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title='IPL_Score_Predictor',layout="centered")
 
 #Get the ML model 
 
-filename='ml_model.pkl.gz'
+filename='ml_model.pkl'
 model = pickle.load(open(filename,'rb'))
 
 #Title of the page with CSS
